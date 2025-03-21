21fd876f561d110af292c387b6d36c78f7c614cb
muarts <muartsokucu@gmail.com>
2025-03-21 15:33:22 +0300

    add smoke test

--- src/test/java/com/saucedemo/LoginPageTest.java
+++ src/test/java/com/saucedemo/LoginPageTest.java
@@ -1,4 +1,14 @@
 package com.saucedemo;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class LoginPageTest {
+
+    @Test
+    public void testSmoke() {
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        driver.close();
+    }
 }
