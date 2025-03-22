056e7f5106534f53138b75f5ee73d98fb81172c3
muarts <muartsokucu@gmail.com>
2025-03-22 23:25:40 +0300

    refactor.

--- src/test/java/com/saucedemo/FooterTest.java
+++ src/test/java/com/saucedemo/FooterTest.java
@@ -4,6 +4,7 @@ import com.saucedemo.page.common.Footer;
 import org.testng.annotations.Test;
 
 import static com.saucedemo.testdata.CommonConstant.FOOTER_TEXT;
+import static com.saucedemo.testdata.CommonConstant.PRODUCTS_PAGE_PATH;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
@@ -12,7 +13,7 @@ public class FooterTest extends TestBase {
     @Test
     public void testFooterTextIsVisible() {
         Footer footer = new Footer(driver);
-        footer.bypassLoginAndGetHere("/inventory.html");
+        footer.bypassLoginAndGetHere(PRODUCTS_PAGE_PATH);
         assertThat(footer.getFooterText(), is(equalTo(FOOTER_TEXT)));
     }
 }
